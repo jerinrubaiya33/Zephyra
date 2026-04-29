007307051c304edebdc66858a438d9666d5a6c88
Rubaiya Khan Jerin <163726222+jerinrubaiya33@users.noreply.github.com>
2026-04-29 21:21:08 +0600

    Refactor Home component and restore imports
    Reorganized the Home component structure and restored necessary imports.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,70 +1,3 @@
-// import React, { useRef } from 'react';
-// import Hero from '../components/Hero';
-// import BestSeller from '../components/BestSeller';
-// import OurPolicy from '../components/OurPolicy';
-// import NewsletterBox from '../components/NewsletterBox';
-// import LatestCollection from '../components/LatestCollection';
-// import Categories from '../components/Categories';
-// import Banner from '../components/Banner';
-
-// const Home = () => {
-//   const bestSellerRef = useRef(null);
-//   const latestCollectionRef = useRef(null);
-//   const categoriesRef = useRef(null);
-
-//   const scrollToBestSeller = () => {
-//     bestSellerRef.current?.scrollIntoView({ behavior: 'smooth' });
-//   };
-
-//   const scrollToLatest = () => {
-//     latestCollectionRef.current?.scrollIntoView({ behavior: 'smooth' });
-//   };
-
-//   const scrollToCategories = () => {
-//     categoriesRef.current?.scrollIntoView({ behavior: 'smooth' });
-//   };
-// //fcf1ff pruple
-//   return (
-//     <div className='bg-[#fff3f7]'>
-//       <Hero
-//         scrollToBestSeller={scrollToBestSeller}
-//         scrollToLatest={scrollToLatest}
-//         scrollToCategories={scrollToCategories}
-//       />
-
-//       {/* Categories Section */}
-//       <div ref={categoriesRef}>
-//         <Categories />
-//       </div>
-
-//       {/* Latest Collection Section */}
-//       <div ref={latestCollectionRef}>
-//         <LatestCollection />
-//       </div>
-
-//       {/* Best Seller Section */}
-//       <div ref={bestSellerRef}>
-//         <BestSeller />
-//       </div>
-
-//       {/* Banner Section */}
-//       <Banner />
-
-//       <OurPolicy />
-//       <NewsletterBox />
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-
-
-
-
-
-
 
 
 import React, { useRef } from 'react';
@@ -106,12 +39,12 @@ const Home = () => {
         scrollToCategories={scrollToCategories}
       />
 
-      <div ref={categoriesRef}>
-        <Categories />
-      </div>
-
       <div ref={latestCollectionRef}>
         <LatestCollection />
+      </div>
+
+      <div ref={categoriesRef}>
+        <Categories />
       </div>
 
       <div ref={bestSellerRef}>
